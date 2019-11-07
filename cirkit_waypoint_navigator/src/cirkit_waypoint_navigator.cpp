71f0713890bfd676333afd315eafe7cd7eb649b3
Koichi Hikawa <rkoichi2001@gmail.com>
2019-11-08 07:03:18 +0900

    waypoint mgr obst detection

--- cirkit_waypoint_navigator/src/cirkit_waypoint_navigator.cpp
+++ cirkit_waypoint_navigator/src/cirkit_waypoint_navigator.cpp
@@ -11,6 +11,7 @@ read_csv.cpp : https://gist.github.com/yoneken/5765597#file-read_csv-cpp
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 #include <laser_geometry/laser_geometry.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <nav_msgs/OccupancyGrid.h>
 #include <ros/package.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
@@ -21,15 +22,20 @@ read_csv.cpp : https://gist.github.com/yoneken/5765597#file-read_csv-cpp
 #include <boost/shared_array.hpp>
 #include <boost/tokenizer.hpp>
 
-#include <stdio.h>
+#include <opencv2/core.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/highgui.hpp>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <stdio.h>
 #include <string>
 
-#include "ros_colored_msg.h"  // FIXME: this header depend ROS, but exclude ros header. Now must be readed after #include"ros/ros.h"
+#include "ros_colored_msg.h" // FIXME: this header depend ROS, but exclude ros header. Now must be readed after #include"ros/ros.h"
 
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>
+    MoveBaseClient;
 
 typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
 
@@ -48,20 +54,20 @@ enum WayPointType {
 };
 
 class WayPoint {
- public:
+public:
   WayPoint();
-  WayPoint(move_base_msgs::MoveBaseGoal goal, WayPointType waypoint_type, double reach_threshold)
-      : goal_(goal),
-        waypoint_type_(waypoint_type),
-        reach_threshold_(reach_threshold),
-        user_input_received_(false) {}
-  ~WayPoint() {}  // FIXME: Don't declare destructor!!
+  WayPoint(move_base_msgs::MoveBaseGoal goal, WayPointType waypoint_type,
+           double reach_threshold)
+      : goal_(goal), waypoint_type_(waypoint_type),
+        reach_threshold_(reach_threshold), user_input_received_(false) {}
+  ~WayPoint() {} // FIXME: Don't declare destructor!!
 
   WayPointType GetWayPointType() { return waypoint_type_; }
   bool WaitUserInput() {
     bool tmp_user_input_received_ = user_input_received_;
     user_input_received_ = true;
-    return waypoint_type_ == WayPointType::WAIT_USER_INPUT && !tmp_user_input_received_;
+    return waypoint_type_ == WayPointType::WAIT_USER_INPUT &&
+           !tmp_user_input_received_;
   }
   move_base_msgs::MoveBaseGoal goal_;
   WayPointType waypoint_type_;
@@ -70,14 +76,15 @@ class WayPoint {
 };
 
 class CirkitWaypointNavigator {
- public:
+public:
   CirkitWaypointNavigator();
 
   ~CirkitWaypointNavigator();
 
   void SendNewGoal(geometry_msgs::Pose pose);
 
-  void SendNextWaypointMarker(const geometry_msgs::Pose waypoint, int target_object_mode);
+  void SendNextWaypointMarker(const geometry_msgs::Pose waypoint,
+                              int target_object_mode);
 
   void CancelGoal();
 
@@ -87,9 +94,10 @@ class CirkitWaypointNavigator {
 
   bool IsFinalGoal();
 
-  double ComputeDistanceToWaypoint(geometry_msgs::Pose a, geometry_msgs::Pose b);
+  double ComputeDistanceToWaypoint(geometry_msgs::Pose a,
+                                   geometry_msgs::Pose b);
 
-  void SendNextGoal(const WayPoint& waypoint);
+  void SendNextGoal(const WayPoint &waypoint);
 
   double GetWaypointReachThreshold();
 
@@ -97,37 +105,44 @@ class CirkitWaypointNavigator {
 
   geometry_msgs::Pose GetCurrentGoalPosition();
 
+  void HandleLocalCostMap(const nav_msgs::OccupancyGridConstPtr &grid_map);
+
   void Run();
 
- private:
+private:
+  ros::NodeHandle nh_;
   MoveBaseClient ac_;
   RobotBehaviors::State robot_behavior_state_;
   ros::Rate rate_;
   std::vector<std::shared_ptr<WayPoint>> waypoints_buff_;
-  ros::NodeHandle nh_;
   tf::TransformListener listener_;
-  int target_waypoint_index_;           // 次に目指すウェイポイントのインデックス
-  double dist_thres_to_target_object_;  // 探索対象にどれだけ近づいたらゴールとするか
-  double reach_threshold_;  // 今セットされてるゴール（waypointもしくは探索対象）へのしきい値
-  geometry_msgs::Pose now_goal_;  // 現在目指しているゴールの座標
-  ros::Publisher cmd_vel_pub_;
-  ros::Publisher next_waypoint_marker_pub_;
+  int target_waypoint_index_;
+  double reach_threshold_;
+  geometry_msgs::Pose now_goal_; // 現在目指しているゴールの座標
+  ros::Publisher cmd_vel_pub_, next_waypoint_marker_pub_;
+  ros::Subscriber local_cost_map_sub_;
 };
 
-CirkitWaypointNavigator::CirkitWaypointNavigator() : ac_("move_base", true), rate_(10) {
+CirkitWaypointNavigator::CirkitWaypointNavigator()
+    : ac_("move_base", true), rate_(10) {
   robot_behavior_state_ = RobotBehaviors::INIT_NAV;
   std::string filename;
-
   ros::NodeHandle n("~");
 
   // Read Parameter.
-  n.param<std::string>("waypointsfile", filename,
-                       ros::package::getPath("cirkit_waypoint_navigator") +
-                           "/waypoints/garden_waypoints.csv");  // FIXME: Don't find!
-  n.param("dist_thres_to_target_object", dist_thres_to_target_object_, 1.8);
+  n.param<std::string>(
+      "waypointsfile", filename,
+      ros::package::getPath("cirkit_waypoint_navigator") +
+          "/waypoints/garden_waypoints.csv"); // FIXME: Don't find!
   n.param("start_waypoint", target_waypoint_index_, 0);
 
-  next_waypoint_marker_pub_ = nh_.advertise<visualization_msgs::Marker>("/next_waypoint", 1);
+  // X. Setup Publisher.
+  next_waypoint_marker_pub_ =
+      nh_.advertise<visualization_msgs::Marker>("/next_waypoint", 1);
+
+  // X. Setup Subscriber
+  local_cost_map_sub_ = nh_.subscribe(
+      "/local_cost_map", 1, &CirkitWaypointNavigator::HandleLocalCostMap, this);
 
   // X. Reading Way Points.
   ROS_INFO("[Waypoints file name] : %s", filename.c_str());
@@ -150,8 +165,8 @@ void CirkitWaypointNavigator::SendNewGoal(const geometry_msgs::Pose pose) {
   now_goal_ = goal.target_pose.pose;
 }
 
-void CirkitWaypointNavigator::SendNextWaypointMarker(const geometry_msgs::Pose waypoint,
-                                                     int target_object_mode) {
+void CirkitWaypointNavigator::SendNextWaypointMarker(
+    const geometry_msgs::Pose waypoint, int target_object_mode) {
   visualization_msgs::Marker waypoint_marker;
   waypoint_marker.header.frame_id = "map";
   waypoint_marker.header.stamp = ros::Time();
@@ -176,13 +191,15 @@ void CirkitWaypointNavigator::CancelGoal() {
 }
 
 int CirkitWaypointNavigator::ReadWaypointFile(std::string filename) {
-  const int rows_num = 9;  // x, y, z, Qx,Qy,Qz,Qw, waypoint_type, reach_threshold
+  const int rows_num =
+      9; // x, y, z, Qx,Qy,Qz,Qw, waypoint_type, reach_threshold
   boost::char_separator<char> sep(",", "", boost::keep_empty_tokens);
   std::ifstream ifs(filename.c_str());
   std::string line;
   while (ifs.good()) {
     getline(ifs, line);
-    if (line.empty()) break;
+    if (line.empty())
+      break;
     tokenizer tokens(line, sep);
     std::vector<double> data;
     tokenizer::iterator it = tokens.begin();
@@ -205,8 +222,8 @@ int CirkitWaypointNavigator::ReadWaypointFile(std::string filename) {
       waypoint.target_pose.pose.orientation.y = data[4];
       waypoint.target_pose.pose.orientation.z = data[5];
       waypoint.target_pose.pose.orientation.w = data[6];
-      waypoints_buff_.push_back(std::shared_ptr<WayPoint>(
-          new WayPoint(waypoint, static_cast<WayPointType>((int)data[7]), data[8] / 2.0)));
+      waypoints_buff_.push_back(std::shared_ptr<WayPoint>(new WayPoint(
+          waypoint, static_cast<WayPointType>((int)data[7]), data[8] / 2.0)));
     }
   }
   return 0;
@@ -225,20 +242,24 @@ bool CirkitWaypointNavigator::IsFinalGoal() {
   }
 }
 
-double CirkitWaypointNavigator::ComputeDistanceToWaypoint(geometry_msgs::Pose a,
-                                                          geometry_msgs::Pose b) {
-  return sqrt(pow((a.position.x - b.position.x), 2.0) + pow((a.position.y - b.position.y), 2.0));
+double
+CirkitWaypointNavigator::ComputeDistanceToWaypoint(geometry_msgs::Pose a,
+                                                   geometry_msgs::Pose b) {
+  return sqrt(pow((a.position.x - b.position.x), 2.0) +
+              pow((a.position.y - b.position.y), 2.0));
 }
 
 // 通常のwaypointの場合
-void CirkitWaypointNavigator::SendNextGoal(const WayPoint& waypoint) {
+void CirkitWaypointNavigator::SendNextGoal(const WayPoint &waypoint) {
   reach_threshold_ = waypoint.reach_threshold_;
   this->SendNextWaypointMarker(waypoint.goal_.target_pose.pose,
-                               0);  // 現在目指しているwaypointを表示する
+                               0); // 現在目指しているwaypointを表示する
   this->SendNewGoal(waypoint.goal_.target_pose.pose);
 }
 
-double CirkitWaypointNavigator::GetWaypointReachThreshold() { return reach_threshold_; }
+double CirkitWaypointNavigator::GetWaypointReachThreshold() {
+  return reach_threshold_;
+}
 
 geometry_msgs::Pose CirkitWaypointNavigator::GetRobotCurrentPosition() {
   // tfを使ってロボットの現在位置を取得する
@@ -254,7 +275,34 @@ geometry_msgs::Pose CirkitWaypointNavigator::GetRobotCurrentPosition() {
   return pose;
 }
 
-geometry_msgs::Pose CirkitWaypointNavigator::GetCurrentGoalPosition() { return now_goal_; }
+geometry_msgs::Pose CirkitWaypointNavigator::GetCurrentGoalPosition() {
+  return now_goal_;
+}
+
+void CirkitWaypointNavigator::HandleLocalCostMap(
+    const nav_msgs::OccupancyGridConstPtr &grid_map) {
+  ROS_ERROR("Receive OGM");
+  ROS_ERROR("Width : %u, Height : %u", grid_map->info.width,
+            grid_map->info.height);
+  ROS_ERROR("Origin.x : %lf, Origin.y : %lf", grid_map->info.origin.position.x,
+            grid_map->info.origin.position.y);
+  ROS_ERROR("Resolution : %lf", grid_map->info.resolution);
+  ROS_ERROR("Frame ID : %s", grid_map->header.frame_id.c_str());
+  double yaw = tf::getYaw(grid_map->info.origin.orientation);
+  ROS_ERROR("Yaw : %lf", yaw);
+  ROS_ERROR("Size : %d", grid_map->data.size());
+
+  cv::Mat mat = cv::Mat::zeros(120, 120, CV_8SC1);
+
+  for (int j = 0; j < 120; j++) {
+    for (int i = 0; i < 120; i++) {
+      mat.at<int8_t>(j, i) = grid_map->data[120 * i + j];
+    }
+  }
+
+  cv::imshow("Test", mat);
+  cv::waitKey(1);
+}
 
 void CirkitWaypointNavigator::Run() {
   robot_behavior_state_ = RobotBehaviors::INIT_NAV;
@@ -282,34 +330,38 @@ void CirkitWaypointNavigator::Run() {
     ros::Time cur_waypnt_start_time = ros::Time::now();
     ros::Time verbose_start = ros::Time::now();
     double last_distance_to_goal = 0;
-    double delta_distance_to_goal = 1.0;  // 0.1[m]より大きければよい
+    double delta_distance_to_goal = 1.0; // 0.1[m]より大きければよい
 
     // X. Loop till robot state changed.
     while (ros::ok()) {
-      geometry_msgs::Pose robot_current_position = this->GetRobotCurrentPosition();
-      geometry_msgs::Pose current_goal_position = this->GetCurrentGoalPosition();
+      geometry_msgs::Pose robot_current_position =
+          this->GetRobotCurrentPosition();
+      geometry_msgs::Pose current_goal_position =
+          this->GetCurrentGoalPosition();
 
       // X. Compute distance to reach targeted waypoint.
-      double distance_to_goal =
-          this->ComputeDistanceToWaypoint(robot_current_position, current_goal_position);
+      double distance_to_goal = this->ComputeDistanceToWaypoint(
+          robot_current_position, current_goal_position);
 
       // X. Judge if robot is stuck.
       delta_distance_to_goal = last_distance_to_goal - distance_to_goal;
       if (delta_distance_to_goal < 0.1) {
         // X. Judge based on spent time in current waypoint.
-        ros::Duration spent_time_in_cur_waypnt = ros::Time::now() - cur_waypnt_start_time;
+        ros::Duration spent_time_in_cur_waypnt =
+            ros::Time::now() - cur_waypnt_start_time;
         if (spent_time_in_cur_waypnt.toSec() > 10.0) {
           if (robot_behavior_state_ == RobotBehaviors::WAYPOINT_NAV) {
-            robot_behavior_state_ =
-                RobotBehaviors::WAYPOINT_NAV_PLANNING_ABORTED;  // プランニング失敗とする
+            robot_behavior_state_ = RobotBehaviors::
+                WAYPOINT_NAV_PLANNING_ABORTED; // プランニング失敗とする
             break;
           } else {
             break;
           }
-        } else {  // 30秒おきに進捗を報告する
+        } else { // 30秒おきに進捗を報告する
           ros::Duration verbose_time = ros::Time::now() - verbose_start;
           if (verbose_time.toSec() > 30.0) {
-            ROS_INFO_STREAM("Waiting Abort: passed 30s, Distance to goal: " << distance_to_goal);
+            ROS_INFO_STREAM("Waiting Abort: passed 30s, Distance to goal: "
+                            << distance_to_goal);
             verbose_start = ros::Time::now();
           }
         }
@@ -335,33 +387,33 @@ void CirkitWaypointNavigator::Run() {
 
     // X. Decide next action.
     switch (robot_behavior_state_) {
-      case RobotBehaviors::WAYPOINT_REACHED_GOAL: {
-        ROS_INFO("WAYPOINT_REACHED_GOAL");
-        // If this is final waypoint, cancel goal and terminate.
-        if (this->IsFinalGoal()) {
-          this->CancelGoal();
-          return;
-        }
-        break;
-      }
-      case RobotBehaviors::WAYPOINT_NAV_PLANNING_ABORTED: {
-        ROS_INFO("!! WAYPOINT_NAV_PLANNING_ABORTED !!");
-        // Cancel current goal.
+    case RobotBehaviors::WAYPOINT_REACHED_GOAL: {
+      ROS_INFO("WAYPOINT_REACHED_GOAL");
+      // If this is final waypoint, cancel goal and terminate.
+      if (this->IsFinalGoal()) {
         this->CancelGoal();
-        target_waypoint_index_ -= 1;
-        break;
+        return;
       }
-      default: {
-        ROS_WARN_STREAM("!! UNKNOWN STATE !!");
-        break;
-      }
+      break;
+    }
+    case RobotBehaviors::WAYPOINT_NAV_PLANNING_ABORTED: {
+      ROS_INFO("!! WAYPOINT_NAV_PLANNING_ABORTED !!");
+      // Cancel current goal.
+      this->CancelGoal();
+      target_waypoint_index_ -= 1;
+      break;
+    }
+    default: {
+      ROS_WARN_STREAM("!! UNKNOWN STATE !!");
+      break;
+    }
     }
     rate_.sleep();
     ros::spinOnce();
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ros::init(argc, argv, "cirkit_waypoint_navigator");
   CirkitWaypointNavigator cirkit_waypoint_navigator;
   cirkit_waypoint_navigator.Run();
